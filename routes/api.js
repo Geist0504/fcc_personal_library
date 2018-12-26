d5830403de0ce74cfc8415ef1e49b906accbc818
Glitch (magenta-beer) <none>
2018-12-26 16:33:10 +0000

    🙀🗼 Checkpoint ./routes/api.js:2025103/888

--- routes/api.js
+++ routes/api.js
@@ -14,6 +14,8 @@ var ObjectId = require('mongodb').ObjectId;
 const MONGODB_CONNECTION_STRING = process.env.DB;
 //Example connection: MongoClient.connect(MONGODB_CONNECTION_STRING, function(err, db) {});
 
+const collection = 'books'
+
 module.exports = function (app) {
 
   app.route('/api/books')
@@ -21,14 +23,24 @@ module.exports = function (app) {
       //response will be array of book objects
       //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
       MongoClient.connect(MONGODB_CONNECTION_STRING, (err, db) => {
-        let collection = db.collection('books')
-        console.log(collection.find())
-        res.send('working')
+        let collection = db.collection(collection)
+        collection.find().toArray((err,docs) => {res.json(docs)})
       });
     })
     
     .post(function (req, res){
-      var title = req.body.title;
+      if(!req.body.title){
+        res.send('missing title')
+      } else{
+        let book = {title: req.body.title}
+        MongoClient.connect(MONGODB_CONNECTION_STRING, (err, db) => {
+          let collection = db.collection(collection)
+          collection.insertOne(book, (err, data) => {
+            book._id = data.insertedId;
+            res.json(book)
+          })
+        })
+      }  
       //response will contain new book object including atleast _id and title
     })
     
